7b4f60c9527bce411b050132610bd3fcf8d1a665
MapleLeafKiller <hhuwoyaoxuexi@163.com>
2019-09-28 14:41:22 +0800

    c

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -1,13 +1,28 @@
 import numpy as np
-
+from tensorflow.python.keras.datasets import mnist, cifar10
+from tensorflow.python.keras.utils import to_categorical
 
 class DataGenerator:
     def __init__(self, config):
         self.config = config
         # load data here
-        self.input = np.ones((500, 784))
-        self.y = np.ones((500, 10))
+        self.X_train = np.ones((500, 784))
+        self.y_train = np.ones((500, 10))
+
+    def load_mnist(self):
+        (X_train, y_train), (X_test, y_test) = mnist.load_data()
+        print("shape: X_train:{},y_train:{},X_test:{},y_test:{}".
+              format(X_train.shape, y_train.shape, X_test.shape, y_test.shape, ))
+
+        # for tf.layers.conv2d, input has to be 4 dim:(batch,height,width,channels), for mnist channels=1
+        X_train, X_test = np.expand_dims(X_train / 255.0, axis=-1), np.expand_dims(X_test / 255.0, axis=-1)
+        y_train, y_test = to_categorical(y_train), to_categorical(y_test)
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
 
     def next_batch(self, batch_size):
         idx = np.random.choice(500, batch_size)
-        yield self.input[idx], self.y[idx]
+        yield self.X_train[idx], self.y_train[idx]
--- models/cnn_mnist_model.py
+++ models/cnn_mnist_model.py
@@ -2,24 +2,34 @@ from base.base_model import BaseModel
 import tensorflow as tf
 
 
-class ExampleModel(BaseModel):
+class CnnMnistModel(BaseModel):
     def __init__(self, config):
-        super(ExampleModel, self).__init__(config)
+        super(CnnMnistModel, self).__init__(config)
         self.build_model()
         self.init_saver()
 
     def build_model(self):
         self.is_training = tf.placeholder(tf.bool)
 
-        self.x = tf.placeholder(tf.float32, shape=[None] + self.config.state_size)
+        self.x = tf.placeholder(tf.float32, shape=[None, self.config.pixel_size, self.config.pixel_size, 1])
         self.y = tf.placeholder(tf.float32, shape=[None, 10])
 
         # network architecture
         # d1 = tf.layers.dense(self.x, 512, activation=tf.nn.relu, name="dense1")
-        d1 = tf.layers.dense(self.x, 128, activation=tf.nn.relu, name="dense1")
-        d2 = tf.layers.dense(d1, 10, name="dense2")
+
+        with tf.name_scope("hidden"):
+            out = tf.layers.conv2d(self.x, filters=5, kernel_size=3, padding='same', name="conv1", activation=tf.nn.relu)
+            # out = tf.nn.relu(out)
+            out = tf.layers.max_pooling2d(out, 2, 2)
+
+            assert out.shape[1:] == [14, 14, 5]
+            out = tf.reshape(out, [-1, 14 * 14 * 5])
+
+            d1 = tf.layers.dense(out, 128, activation=tf.nn.relu, name="dense1")
+            d2 = tf.layers.dense(d1, 10, name="dense2")
 
         with tf.name_scope("loss"):
+            print("shape:self.y:{},d2:{}".format(self.y.shape, d2.shape))
             self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=d2))
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
@@ -31,5 +41,5 @@ class ExampleModel(BaseModel):
 
     def init_saver(self):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
-        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_ckpt_to_keep)
 
